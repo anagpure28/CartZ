effa277b1a5289702c04f39a385849af13581de4
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-04-22 15:08:09 +0530

    Product reducer

---
+++ cartz/src/Redux/ProductReducer/reducer.js
@@ -0,0 +1,22 @@
+import { GET_PRODUCT_SUCCESS, POST_PRODUCT_SUCCESS, PRODUCT_FAILURE, PRODUCT_REQUEST } from "../actionType"
+
+const initialState = {
+    isLoading: "",
+    isError: "",
+    products: []
+}
+
+export const reducer = (state=initialState, {type, payload}) => {
+    switch(type){
+        case PRODUCT_REQUEST: 
+            return {...state, isLoading: true}
+        case GET_PRODUCT_SUCCESS: 
+            return {...state, isLoading: false, products: payload}
+        case POST_PRODUCT_SUCCESS: 
+            return {...state, isLoading: false}
+        case PRODUCT_FAILURE: 
+            return {...state, isLoading: false, isError: true}
+        default: 
+            return state;
+    }
+}
