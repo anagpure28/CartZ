3b695e7b5b0a300b5dc7717a1431e1d8772403bc
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-05-31 05:04:58 +0530

    Mens and Womens Pages Added

--- cartz/src/Products/ProductPages/MenProducts.jsx
+++ cartz/src/Products/ProductPages/MenProducts.jsx
@@ -1,8 +1,50 @@
-function MenProducts(){
-    return <div>
-        <h1>
-            Men Products
-        </h1>
-    </div>
-}
-export default MenProducts;
+import styled from "styled-components";
+import { SideBar } from "../../Components/SideBar";
+import { MenProductList } from "../ProductList/MenProductList";
+
+export const MenProducts = () => {
+  return (
+    <DIV>
+      <div className="banner">
+      </div>
+      <div className="flex">
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="allProductList">
+          <MenProductList />
+        </div>
+      </div>
+    </DIV>
+  );
+};
+
+const DIV = styled.div`
+    .flex{
+        width: 90%;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+    }
+    .sidebar{
+        width: 20%;
+        margin-top: 40px;
+        padding-right: 12px;
+        border-right: 1px solid black;
+    }
+    .allProductList{
+        width: 78%;
+        margin-top: 40px;
+        /* border: 1px solid black; */
+    }
+    .banner{
+        width: 95%;
+        height: 350px;
+        margin: auto;
+        margin-top: 62px;
+        background-image: url("https://marketplace.canva.com/EAFWt8Wq208/1/0/1600w/canva-grey-minimalist-special-offer-banner-landscape-tVz4E4KVLgk.jpg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        position: relative;
+    }
+`;
