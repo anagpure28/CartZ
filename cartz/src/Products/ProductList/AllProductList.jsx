26263e16f888911666b81b42804ca8bae3618a4d
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-05-31 03:08:01 +0530

    Added Search Functionality

--- cartz/src/Products/ProductList/AllProductList.jsx
+++ cartz/src/Products/ProductList/AllProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { mensProduct } from "../../Redux/ProductReducer/action";
 import { AllProductCard } from "./AllProductCard";
@@ -7,12 +7,13 @@ import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import { useLocation, useSearchParams } from "react-router-dom";
 
 export const AllProductList = () => {
-  const [searchParams] = useSearchParams()
+  const [query, setQuery] = useState("");
+  const [searchParams] = useSearchParams();
   const products = useSelector((store) => store.ProductReducer.products);
   const loading = useSelector((store) => store.ProductReducer.isLoading);
-  const location = useLocation()
+  const location = useLocation();
   const dispatch = useDispatch();
-  console.log(products);
+  let ref = useRef();
 
   let obj = {
     params: {
@@ -22,10 +23,28 @@ export const AllProductList = () => {
     }
   }
 
+  const paramObj = {
+    params: {
+      q: query && query,
+    },
+  };
+
+  //Fetching Data
   useEffect(() => {
     console.log("data", obj)
     dispatch(mensProduct(obj));
   }, [location.search]);
+
+  //Search functionality
+  useEffect(() => {
+    if (ref.current) {
+      clearTimeout(ref.current);
+    }
+
+    ref.current = setTimeout(() => {
+      dispatch(mensProduct(paramObj));
+    }, 1000);
+  }, [query]);
 
   if(loading){
     return <Box padding="6" boxShadow="lg" bg="white">
@@ -36,15 +55,45 @@ export const AllProductList = () => {
 
   return (
     <DIV>
-      {products.length > 0 && products.map((el) => {
-        return <AllProductCard key={el.id} {...el} />;
-      })}
+      <div className="input">
+        <input
+          type="text"
+          id="search"
+          autoComplete="off"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      {/* {loading ? <span>Loading</span> : */}
+      <div className="grid">
+        {products.length > 0 &&
+          products.map((el) => {
+            return <AllProductCard key={el.id} {...el} />;
+          })}
+      </div> 
+      {/* } */}
     </DIV>
   );
 };
 
 const DIV = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 20px;
+  text-align: left;
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+  }
+  #search {
+    border: 3px solid #242424;
+    border-radius: 10px;
+    width: 30%;
+    color: black;
+    padding: 6px 10px;
+  }
+  img {
+    margin: 10px 0;
+  }
+  .input{
+    margin: 20px 0;
+  }
 `;
