067828c8172c12ebfdb4931a5eb80a9a7398b823
Aniket Zade <aniketzade4024@gmail.com>
2023-05-31 23:48:38 +0530

    new collection done

--- cartz/src/App.js
+++ cartz/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Navbar1/>
-      <AllRoutes />
+      <Navbar1 />
+      <AllRoutes/>
       <Footer />
     </div>
   );
