f13641e949c968e51b49e1498302561b076815ed
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-06-01 01:14:00 +0530

    SingleProductPage fixes

--- cartz/src/App.js
+++ cartz/src/App.js
@@ -3,13 +3,15 @@ import AllRoutes from './Components/AllRoutes';
 import Navbar from './Components/Navbar';
 import Navbar1 from './Components/Navbar1';
 import Footer from "../src/Footer/Footer";
+import SingleProductPage from './Pages/SingleProductPage';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       <Navbar1 />
-      <AllRoutes/>
+      {/* <AllRoutes/> */}
+      <SingleProductPage />
       <Footer />
     </div>
   );
