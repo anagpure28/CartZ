100973be3c3cc80ea746786698939a6d7b5ad6fc
anagpure28 <aniketnagpureofficial@gmail.com>
2023-10-15 00:50:38 +0530

    Not Found page edited

--- cartz/src/Pages/NotFound.jsx
+++ cartz/src/Pages/NotFound.jsx
@@ -1,34 +1,17 @@
-import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { Button, Result } from 'antd';
 
 export default function NotFound() {
   return (
     <div>
-      <Box textAlign="center" py={250} px={6}>
-        <Heading
-          display="inline-block"
-          as="h2"
-          size="2xl"
-          // mt={"4%"}
-          bgGradient="linear(to-r, pink.400, orange.400)"
-          backgroundClip="text">
-          404
-        </Heading>
-        <Text fontSize="18px" mt={3} mb={2}>
-          Page Not Found
-        </Text>
-        <Text color={'gray.500'} mb={6}>
-          The page you're looking for does not seem to exist
-        </Text>
-        <Link to="/">
-          <Button
-            colorScheme="orange"
-            bgGradient="linear(to-r, pink.400, orange.400)"
-            color="white"
-            variant="solid">
-            Go to Home
-          </Button>
-        </Link>
+      <Box textAlign="center" py={70} px={6}>
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the server cannot find the requested resource"
+          extra={<Link to="/"><Button type="primary">Back Home</Button></Link>}
+        />
       </Box>
     </div>
   )
