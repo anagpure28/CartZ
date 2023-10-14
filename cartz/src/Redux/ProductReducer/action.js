43907a03cd7a68d5c0860668c83ddf0795256a0f
anagpure28 <aniketnagpureofficial@gmail.com>
2023-10-15 03:11:58 +0530

    Boys and girls api added

--- cartz/src/Redux/ProductReducer/action.js
+++ cartz/src/Redux/ProductReducer/action.js
@@ -4,7 +4,7 @@ import { GET_PRODUCT_SUCCESS, PRODUCT_FAILURE, PRODUCT_REQUEST } from "../action
 //Mens Product
 export const mensProduct = (data) => (dispatch) => {
     dispatch({type: PRODUCT_REQUEST})
-    axios.get(process.env.REACT_MEN_API, data).then((res)=> {
+    axios.get("https://845wro.sse.codesandbox.io/Men", data).then((res)=> {
         dispatch({type: GET_PRODUCT_SUCCESS, payload: res.data})
     }).catch((err)=> {
         dispatch({type: PRODUCT_FAILURE})
@@ -24,7 +24,7 @@ export const womensProduct = (data) => (dispatch) => {
 //Boys Product
 export const boysProduct = (data) => (dispatch) => {
     dispatch({type: PRODUCT_REQUEST})
-    axios.get("https://00ty7r-8080.csb.app/boys", data).then((res)=> {
+    axios.get("https://845wro.sse.codesandbox.io/Boys", data).then((res)=> {
         dispatch({type: GET_PRODUCT_SUCCESS, payload: res.data})
     }).catch((err)=> {
         dispatch({type: PRODUCT_FAILURE})
@@ -34,7 +34,7 @@ export const boysProduct = (data) => (dispatch) => {
 // Girls Product
 export const girlsProduct = (data) => (dispatch) => {
     dispatch({type: PRODUCT_REQUEST})
-    axios.get("https://00ty7r-8080.csb.app/girls", data).then((res)=> {
+    axios.get("https://845wro.sse.codesandbox.io/Girls", data).then((res)=> {
         dispatch({type: GET_PRODUCT_SUCCESS, payload: res.data})
     }).catch((err)=> {
         dispatch({type: PRODUCT_FAILURE})
