ee310049474600a8d053c5a4501e056120016523
anagpure28 <aniketnagpureofficial@gmail.com>
2023-10-18 00:10:49 +0530

    Contact page done

--- cartz/src/Pages/Contact.jsx
+++ cartz/src/Pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Container,
   Flex,
@@ -17,147 +17,192 @@ import {
   InputGroup,
   InputLeftElement,
   Textarea,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 import {
   MdPhone,
   MdEmail,
   MdLocationOn,
   MdFacebook,
   MdOutlineEmail,
-} from 'react-icons/md'
-import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
+} from "react-icons/md";
+import { BsGithub, BsDiscord, BsPerson } from "react-icons/bs";
+import { useEffect } from "react";
 
-export const Contact = () => {
+const Contact = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
-      <Flex>
-        <Box
-          bg="#02054B"
-          color="white"
-          borderRadius="lg"
-          m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ sm: 5, md: 5, lg: 16 }}>
-          <Box p={4}>
-            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-              <WrapItem>
-                <Box>
-                  <Heading>Contact</Heading>
-                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                    Fill up the form below to contact
-                  </Text>
-                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                        +91-9209216409
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="260px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                        aniketnagpure28@gmail.com
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                        Maharashtra, India
-                      </Button>
-                    </VStack>
-                  </Box>
-                  <HStack
-                    mt={{ lg: 10, md: 10 }}
-                    spacing={5}
-                    px={5}
-                    alignItems="flex-start">
-                    <IconButton
-                      aria-label="facebook"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<MdFacebook size="28px" />}
-                    />
-                    <a href="https://github.com/anagpure28" target="_blank">
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<BsGithub size="28px" />}
-                    />
-                    </a>
-                    <IconButton
-                      aria-label="discord"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<BsDiscord size="28px" />}
-                    />
-                  </HStack>
-                </Box>
-              </WrapItem>
-              <WrapItem>
-                <Box bg="white" borderRadius="lg">
-                  <Box m={8} color="#0B0E3F">
-                    <VStack spacing={5}>
-                      <FormControl id="name">
-                        <FormLabel>Your Name</FormLabel>
-                        <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement pointerEvents="none">
-                            <BsPerson color="gray.800" />
-                          </InputLeftElement>
-                          <Input type="text" size="md" />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Mail</FormLabel>
-                        <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement pointerEvents="none">
-                            <MdOutlineEmail color="gray.800" />
-                          </InputLeftElement>
-                          <Input type="text" size="md" />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                          borderColor="gray.300"
-                          _hover={{
-                            borderRadius: 'gray.300',
-                          }}
-                          placeholder="message"
-                        />
-                      </FormControl>
-                      <FormControl id="name" float="right">
-                        <Button variant="solid" bg="#0D74FF" color="white" _hover={{}}>
-                          Send Message
+    <div>
+      <div style={{ marginTop: "70px" }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119064.90027629164!2d78.99010822566491!3d21.16122599657696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0a5a31faf13%3A0x19b37d06d0bb3e2b!2sNagpur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1697565182071!5m2!1sen!2sin"
+          width="100%"
+          height="450"
+          style={{ border: "0px" }}
+          allowFullFcreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <Container
+        bg="#9DC4FB"
+        maxW="full"
+        mt={0}
+        centerContent
+        overflow="hidden"
+      >
+        <Flex>
+          <Box
+            bg="#02054B"
+            color="white"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}
+          >
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem>
+                  <Box>
+                    <Heading>Contact</Heading>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
+                      Fill up the form below to contact
+                    </Text>
+                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                      <VStack pl={0} spacing={3} alignItems="flex-start">
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: "2px solid #1C6FEB" }}
+                          leftIcon={<MdPhone color="#1970F1" size="20px" />}
+                        >
+                          +91-9209216409
                         </Button>
-                      </FormControl>
-                    </VStack>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="260px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: "2px solid #1C6FEB" }}
+                          leftIcon={<MdEmail color="#1970F1" size="20px" />}
+                        >
+                          aniketnagpure28@gmail.com
+                        </Button>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="ghost"
+                          color="#DCE2FF"
+                          _hover={{ border: "2px solid #1C6FEB" }}
+                          leftIcon={
+                            <MdLocationOn color="#1970F1" size="20px" />
+                          }
+                        >
+                          Maharashtra, India
+                        </Button>
+                      </VStack>
+                    </Box>
+                    <HStack
+                      mt={{ lg: 10, md: 10 }}
+                      spacing={5}
+                      px={5}
+                      alignItems="flex-start"
+                    >
+                      <IconButton
+                        aria-label="facebook"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: "#0D74FF" }}
+                        icon={<MdFacebook size="28px" />}
+                      />
+                      <a href="https://github.com/anagpure28" target="_blank">
+                        <IconButton
+                          aria-label="github"
+                          variant="ghost"
+                          size="lg"
+                          isRound={true}
+                          _hover={{ bg: "#0D74FF" }}
+                          icon={<BsGithub size="28px" />}
+                        />
+                      </a>
+                      <IconButton
+                        aria-label="discord"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: "#0D74FF" }}
+                        icon={<BsDiscord size="28px" />}
+                      />
+                    </HStack>
                   </Box>
-                </Box>
-              </WrapItem>
-            </Wrap>
+                </WrapItem>
+                <WrapItem>
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <form action="https://formspree.io/f/myyqlvwe" method="POST">
+                        <VStack spacing={5}>
+                          <FormControl id="name">
+                            <FormLabel>Your Name</FormLabel>
+                            <InputGroup borderColor="#E0E1E7">
+                              <InputLeftElement pointerEvents="none">
+                                <BsPerson color="gray.800" />
+                              </InputLeftElement>
+                              <Input type="text" name="Name" placeholder="Name" autocomplete="off" required size="md" />
+                            </InputGroup>
+                          </FormControl>
+                          <FormControl id="name">
+                            <FormLabel>Email</FormLabel>
+                            <InputGroup borderColor="#E0E1E7">
+                              <InputLeftElement pointerEvents="none">
+                                <MdOutlineEmail color="gray.800" />
+                              </InputLeftElement>
+                              <Input type="email" name="Email" placeholder="Email" autocomplete="off" required size="md" />
+                            </InputGroup>
+                          </FormControl>
+                          <FormControl id="name">
+                            <FormLabel>Message</FormLabel>
+                            <Textarea
+                              borderColor="gray.300"
+                              _hover={{
+                                borderRadius: "gray.300",
+                              }}
+                              placeholder="message"
+                              name="message"
+                              autocomplete="off" 
+                              required
+                            />
+                          </FormControl>
+                          <FormControl id="name" float="right">
+                            <Button
+                              variant="solid"
+                              bg="#0D74FF"
+                              color="white"
+                              _hover={{}}
+                              type="submit"
+                              value="send"
+                            >
+                              Send Message
+                            </Button>
+                          </FormControl>
+                        </VStack>
+                      </form>
+                    </Box>
+                  </Box>
+                </WrapItem>
+              </Wrap>
+            </Box>
           </Box>
-        </Box>
-      </Flex>
-    </Container>
-  )
-}
+        </Flex>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
